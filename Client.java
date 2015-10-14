6d4e96a132beb6bf7761a1a8effb7330583c1603
Gokhan <arik.gokhan@yahoo.com.tr>
2015-10-14 19:53:30 +0300

    Update Client.java
    update hostname

--- Client.java
+++ Client.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  *  * Created by Gokhan Arik on 10/8/15.
  *   */
 public class Client {
-    private static final String hostName = "52.91.190.211";
+    private static final String hostName = "";
     private static final int portNumber = 4444;
 
     public static void main(String[] args) throws IOException, InterruptedException {
