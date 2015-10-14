dccfb586f3ed8fd82d0d5978f91129272699931b
Gokhan Arik <garik@clientresourcesinc.com>
2015-10-14 19:52:23 +0300

    Initial commit

---
+++ Client.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *  * Created by Gokhan Arik on 10/8/15.
+ *   */
+public class Client {
+    private static final String hostName = "52.91.190.211";
+    private static final int portNumber = 4444;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ArrayList<MultiClientThread> clients = new ArrayList<>();
+
+        for (int i=0; i < 3; i++){
+            MultiClientThread client = new MultiClientThread(hostName, portNumber);
+            client.start();
+            clients.add(client);
+            Thread.sleep(4000);
+        }
+    }
+}
+
